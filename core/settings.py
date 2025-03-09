675d540a14d124981e3322f2107e31c952be0fc1
Raheem <Kamwangaraheem2050@gmail.com>
2025-03-09 23:33:20 +0300

    Configured Django to use postgres

--- core/settings.py
+++ core/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smart_waste_db1',
+        'USER': 'smart_user1',
+        'PASSWORD': 'Raheem#404',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
