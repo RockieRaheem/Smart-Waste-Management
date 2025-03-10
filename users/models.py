1809e5f780042896d590488b629dfb3aa8521dfc
Raheem <Kamwangaraheem2050@gmail.com>
2025-03-10 10:30:17 +0300

    Modified CustomUser Model to Include Roles

--- users/models.py
+++ users/models.py
@@ -2,8 +2,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15, unique=True)
-    address = models.TextField()
-    
+    ROLE_CHOICES = (
+        ('resident', 'Resident'),
+        ('driver', 'Truck Driver'),
+        ('officer', 'Municipal Officer'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='resident')
+
     def __str__(self):
-        return self.username
+        return f"{self.username} ({self.role})"
+
+
+
