bd16f8b90a9134dfa7d63bfa013bdf29a6064168
Raheem <Kamwangaraheem2050@gmail.com>
2025-03-10 09:46:50 +0300

    Created Serializers for API Data Handling in apps

---
+++ waste/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import GarbageRequest, IllegalDumpingReport
+
+class GarbageRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GarbageRequest
+        fields = '__all__'
+
+class IllegalDumpingReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IllegalDumpingReport
+        fields = '__all__'
