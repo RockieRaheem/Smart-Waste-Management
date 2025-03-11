aceb2ad384b2a7095e3f36ea8de5125c2f0a3b32
Mansoor Madhi Khalid <mansoor.madhikhalid@student.utamu.ac.ug>
2025-03-11 10:58:32 +0300

    Added new login view logic

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from users.models import CustomUser  # Import the user model
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 
 def home_view(request):
     return render(request, 'home.html')
@@ -34,3 +34,17 @@ def signup_view(request):
         return redirect("dashboard")  # Redirect to dashboard
 
     return render(request, "signup.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("dashboard")  # Redirect to dashboard
+        else:
+            return render(request, "login.html", {"error": "Invalid credentials"})
+
+    return render(request, "login.html")
+
