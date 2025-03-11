6f4f83d772a451a71643d0baf0b97dd4a1587024
Mansoor Madhi Khalid <mansoor.madhikhalid@student.utamu.ac.ug>
2025-03-11 10:41:22 +0300

    Created the Dashboard View

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home_view(request):
     return render(request, 'home.html')
 
 def login_view(request):
     return render(request, 'login.html')
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'dashboard.html', {'user': request.user})
