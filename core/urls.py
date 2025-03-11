1869250d631df989cd2fc2b48028540c358cf224
Mansoor Madhi Khalid <mansoor.madhikhalid@student.utamu.ac.ug>
2025-03-11 10:52:32 +0300

    Added the Signup url

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import home_view, login_view, dashboard_view
+from .views import signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,7 +29,9 @@ urlpatterns = [
     path('api/', include('tracking.urls')),
     path('api/', include('routes.urls')),
     path('dashboard/', dashboard_view, name='dashboard'),
+    path('signup/', signup_view, name='signup'),
 ]
+
 
 
 
